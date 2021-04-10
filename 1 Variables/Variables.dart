34a7e1f58a23e36bf065981eed3b680e9e94a35e
DavidCarranza <dcarranza@hotmail.es>
2021-04-10 11:14:22 -0500

    Taller de variables

---
+++ 1 Variables/Variables.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  var nombre ="Juan";
+  var edad = 23;
+  String telefono="333-444-555";
+  int calificacion= 5;
+  
+  print('Nombre: ${nombre}');
+  print('Edad: ${edad}');
+  print('Telefono: $telefono');
+  print('Nota: $calificacion');
+
+
+
+
+  
+}
